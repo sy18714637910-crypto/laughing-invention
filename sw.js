63d8ff24d3a669c46e8f93c74466dae2ad6af803
sy18714637910-crypto <sy18714637910@gmail.com>
2026-07-27 19:30:37 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+﻿const CACHE_NAME = 'habit-tracker-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/db.js',
+  '/js/quotes.js',
+  '/js/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
