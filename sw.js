925eac42d8c3ae8700bdd25ab7bb6abac958a17d
sy18714637910-crypto <sy18714637910@gmail.com>
2026-07-28 09:10:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,5 @@
-﻿const CACHE_NAME = 'habit-tracker-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/db.js',
-  '/js/quotes.js',
-  '/js/app.js',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS).catch(() => {});
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((names) => {
-      return Promise.all(
-        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
-    })
-  );
-});
+﻿const CACHE_NAME = 'habit-tracker-v2';
+const ASSETS = ['/','/打卡.html'];
+self.addEventListener('install',(e)=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS).catch(()=>{})))});
+self.addEventListener('activate',(e)=>{e.waitUntil(caches.keys().then(n=>Promise.all(n.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))))});
+self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
